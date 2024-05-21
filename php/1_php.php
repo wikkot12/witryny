74032038723a00d5080d22a01bd208f57dcbd42a
wikkot12 <161427865+wikkot12@users.noreply.github.com>
2024-05-21 08:30:46 +0200

    Add files via upload

---
+++ php/1_php.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<meta charset="utf-8">
+<title>koterba</title>
+<meta name="keywords' content="szkola, lekcja, temat, nauka, html">
+<meta name="author" content=" Wiktoria Koterba">
+</head>
+</body>
+</html>
