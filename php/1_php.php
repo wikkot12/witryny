82454c597ac4a52d18bbbccc5cff856bf4ef97f7
wikkot12 <161427865+wikkot12@users.noreply.github.com>
2024-05-21 08:58:11 +0200

    Update 1_php.php

--- php/1_php.php
+++ php/1_php.php
@@ -6,5 +6,35 @@
 <meta name="keywords' content="szkola, lekcja, temat, nauka, html">
 <meta name="author" content=" Wiktoria Koterba">
 </head>
+<body>
+<?php
+print("PHP");
+echo ('Zaczynam poznawać PHP');
+echo ("Zaczynam poznawać PHP");
+print ('Zaczynam poznawać PHP');
+print ("Zaczynam poznawać PHP");
+print ("<br>");
+echo ('Zaczynam poznawać PHP');
+print ("<br>");
+echo ("Zaczynam poznawać PHP");
+print ("<br>");
+print ('Zaczynam poznawać PHP');
+print ("<br>");
+print ("Zaczynam poznawać PHP");
+print ("Zaczynam poznawać PHP. "."Lubię PHP.");
+?>
+<?php
+define("SZKOLA", "ZSM");
+print (SZKOLA);
+?>
+<?php
+define ("SZKOLA", "ZSM");
+print (SZKOLA." to moja szkoła.");
+print ("Moja szkoła to ".SZKOLA.".");
+?>
+<?php
+$imie="Anna";
+print $imie;
+?>
 </body>
 </html>
